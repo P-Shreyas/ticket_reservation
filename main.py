9e2d02122b111fa648b7d6b59f78fed00d42ca83
khomesh24 <khomeshthakre24@gmail.com>
2017-10-16 21:58:44 +0530

    added geckodriver

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+
+
 from selenium import webdriver
+from captcha_solver import CaptchaSolver
 
 browser = webdriver.Firefox()
 browser.get("https://www.irctc.co.in/eticketing/loginHome.jsf")
 
-userID=browser.find_element_by_id("usernameId")
-password= browser.find_element_by_name("j_password")
+
+username = "username"
+passwd = "password"
+
+userID=browser.find_element_by_class_name("loginUserId")
+userID.send_keys(username)
+
+password= browser.find_element_by_class_name("loginPassword")
+password.send_keys(passwd)
+
+reload_captcha=browser.find_element_by_class_name("nlpRefresh")
+reload_captcha.click()
+
+captcha_image=browser.find_element_by_id("nlpImgContainer")
